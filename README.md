2e8bee28aafe24b28b45e95e2b1120102a2410bb
Имя Фамилия <manuilov_99@bk.ru>
2025-03-26 22:20:13 +0700

    Добавлены первые пункты документации с публикациями

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # api_final
 api final
+xcxcxcxvs
+gsg
+dgf
+gsg
+
+gs
--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,16 @@
-from django.urls import path
+from django.urls import path, include
+from api.views import PostListView, PostCreateView
+from api.views import PostDetailView, CommentListView
+import djoser
 
 urlpatterns = [
+
+    path('api/v1/posts/', PostListView.as_view(), name='post-list'),
+    path('api/v1/posts/create/', PostCreateView.as_view(), name='post-create'),
+    path('api/v1/posts/<int:pk>/',
+          PostDetailView.as_view(), name='post-detail'),
+    path('api/v1/posts/<int:post_id>/comments/',
+         CommentListView.as_view(), name='post-comments'),
+    path('api/v1/', include('djoser.urls')),
+    path('api/v1/', include('djoser.urls.jwt')),
 ]
